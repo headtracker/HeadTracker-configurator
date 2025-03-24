dff9738efb0b60204d49b2e0291667b9cf2b6347
tuuling <reedik.tuuling@gmail.com>
2025-03-24 17:46:15 +0200

    Inital setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { NgIf } from '@angular/common';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    NgIf
+  ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+    title = 'headtracker-webui';
+
+    @ViewChild(MatSidenav)
+    sidenav!: MatSidenav;
+    isMobile = true;
+
+    constructor(private observer: BreakpointObserver) {
+    }
+
+    ngOnInit() {
+        this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+            if (screenSize.matches) {
+                this.isMobile = true;
+            } else {
+                this.isMobile = false;
+            }
+        });
+    }
+
+    toggleMenu() {
+        if(this.isMobile){
+            this.sidenav.toggle();
+        } else {
+            this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+        }
+    }
+
+}
