25102bdd9394c78d482f80b19cc4a0e8f94a3552
tuuling <reedik.tuuling@gmail.com>
2025-04-13 15:24:22 +0300

    Add notification to use Chromium based browser.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from '@app/app.config';
+import { AppComponent } from '@app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if ('serial' in navigator) {
+  bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+} else {
+  document.querySelector<HTMLDivElement>('#no-serial-box')!.style.display = 'grid';
+  // alert('Web Serial API is not supported in this browser. Please use a compatible browser.');
+}
